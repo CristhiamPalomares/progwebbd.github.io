6c618f93eeda7615e6c5faa553852327733a80a2
CristhiamPalomares <77865911+CristhiamPalomares@users.noreply.github.com>
2021-01-24 01:01:42 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDAnJGpdC1ED9JJ1GYE2vkck4io2_e79wk",
+    authDomain: "progwebbd.firebaseapp.com",
+    projectId: "progwebbd",
+    storageBucket: "progwebbd.appspot.com",
+    messagingSenderId: "141509381950",
+    appId: "1:141509381950:web:fe71a2f9b4ac10be41a66f",
+    measurementId: "G-HRSJWM7TD1"
+  });
